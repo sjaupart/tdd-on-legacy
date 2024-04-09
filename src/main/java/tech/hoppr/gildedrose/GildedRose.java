a5f3c10149afb7d5f163ddbbdf0210c40962fe01
Sébastien JAUPART <35149581+sjaupart@users.noreply.github.com>
2024-04-09 09:01:38 +0200

    move gildedrose kata in a specific package

--- src/main/java/tech/hoppr/gildedrose/GildedRose.java
+++ src/main/java/tech/hoppr/gildedrose/GildedRose.java
@@ -1,4 +1,4 @@
-package tech.hoppr;
+package tech.hoppr.gildedrose;
 
 public class GildedRose {
 
--- src/test/java/tech/hoppr/gildedrose/TexttestFixture.java
+++ src/test/java/tech/hoppr/gildedrose/TexttestFixture.java
@@ -1,4 +1,4 @@
-package tech.hoppr;
+package tech.hoppr.gildedrose;
 
 public class TexttestFixture {
     public static void main(String[] args) {
